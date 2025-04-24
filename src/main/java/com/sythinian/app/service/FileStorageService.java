116d0959ddc302af9a8d07ae1354e154f2e2728a
master312 <sythinian@gmail.com>
2025-04-24 20:14:45 +0200

    Storage service improvements

--- src/main/java/com/sythinian/app/service/FileStorageService.java
+++ src/main/java/com/sythinian/app/service/FileStorageService.java
@@ -5,7 +5,10 @@ import com.sythinian.app.exception.StorageFileNotFoundException;
 import org.springframework.core.io.Resource;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+
 public interface FileStorageService {
     void store(MultipartFile file, String filename) throws StorageException;
+    void store(File file, String filename) throws StorageException;
     Resource load(String filename) throws StorageFileNotFoundException;
 }
--- src/main/java/com/sythinian/app/service/FileSystemFileStorageService.java
+++ src/main/java/com/sythinian/app/service/FileSystemFileStorageService.java
@@ -8,6 +8,7 @@ import org.springframework.core.io.UrlResource;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.MalformedURLException;
@@ -32,19 +33,27 @@ public class FileSystemFileStorageService implements FileStorageService {
 
     @Override
     public void store(MultipartFile file, String filename) {
+        if (file.isEmpty()) {
+            throw new StorageException("Empty file!");
+        }
+
+        Path destinationFile = validateAndResolveDestinationPath(filename);
+        try (InputStream inputStream = file.getInputStream()) {
+            Files.copy(inputStream, destinationFile, StandardCopyOption.REPLACE_EXISTING);
+        } catch (IOException e) {
+            throw new StorageException("Failed to store file.", e);
+        }
+    }
+
+    @Override
+    public void store(File file, String filename) {
+        if (!file.exists()) {
+            throw new StorageException("File does not exist!");
+        }
+
+        Path destinationFile = validateAndResolveDestinationPath(filename);
         try {
-            if (file.isEmpty()) {
-                throw new StorageException("Empty file!");
-            }
-
-            Path destinationFile = this.rootLocation.resolve(Paths.get(filename)).normalize().toAbsolutePath();
-            if (!destinationFile.getParent().equals(this.rootLocation.toAbsolutePath())) {
-                throw new StorageException("No no! Root path invalid.");
-            }
-
-            try (InputStream inputStream = file.getInputStream()) {
-                Files.copy(inputStream, destinationFile, StandardCopyOption.REPLACE_EXISTING);
-            }
+            Files.copy(file.toPath(), destinationFile, StandardCopyOption.REPLACE_EXISTING);
         } catch (IOException e) {
             throw new StorageException("Failed to store file.", e);
         }
@@ -53,11 +62,7 @@ public class FileSystemFileStorageService implements FileStorageService {
     @Override
     public Resource load(String filename) {
         try {
-            Path file = this.rootLocation.resolve(Paths.get(filename)).normalize().toAbsolutePath();
-            if (!file.getParent().equals(this.rootLocation.toAbsolutePath())) {
-                throw new StorageException("Tried to read from invalid location.");
-            }
-
+            Path file = validateAndResolveDestinationPath(filename);
             Resource resource = new UrlResource(file.toUri());
             if (resource.exists() || resource.isReadable()) {
                 return resource;
@@ -67,5 +72,18 @@ public class FileSystemFileStorageService implements FileStorageService {
         } catch (MalformedURLException e) {
             throw new StorageFileNotFoundException("Could not read file: " + filename, e);
         }
+    }
+
+    /**
+     * Validates and resolves the destination path for a file
+     *
+     * @param filename The target filename
+     */
+    private Path validateAndResolveDestinationPath(String filename) {
+        Path destinationFile = this.rootLocation.resolve(Paths.get(filename)).normalize().toAbsolutePath();
+        if (!destinationFile.getParent().equals(this.rootLocation.toAbsolutePath())) {
+            throw new StorageException("Invalid path: attempted to access outside of storage root.");
+        }
+        return destinationFile;
     }
 } 
